2b554511c6d652ec3eea1b528b926b05d9dba34a
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-18 15:02:48 +0900

    .

--- myproject/settings.py
+++ myproject/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 로그인 후 리다이렉트 URL 설정
-LOGIN_REDIRECT_URL = '/posts/'  # 로그인 후 이동할 URL
+# LOGIN_REDIRECT_URL = '/posts/'  # 로그인 후 이동할 URL
 # 로그아웃 후 리다이렉트 URL 설정
-LOGOUT_REDIRECT_URL = '/posts/'  # 로그아웃 후 이동할 URL
+# LOGOUT_REDIRECT_URL = '/posts/'  # 로그아웃 후 이동할 URL
 # 로그인 URL 설정
 LOGIN_URL = '/posts/login/'  # 로그인 페이지 URL
