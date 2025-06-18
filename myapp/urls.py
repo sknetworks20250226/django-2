03d77cceb3422db0f3178d0019ed7628e4aa7d25
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-18 11:52:53 +0900

    .

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list,admin_view
 urlpatterns = [    
     path('', post_list, name='post_list'),
+    path('admin/', admin_view, name='admin_view'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+
+def is_in_group(user):
+    return user.groups.filter(name='admin').exists()
+
+@login_required
+@user_passes_test(is_in_group)
+def admin_view(request):
+    # 관리자 전용 뷰 로직
+    return render(request, 'myapp/admin.html')
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
